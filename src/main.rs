fa9c334d6976a250dae233b4973adced3b86d909
Azanul <azanulhaque@gmail.com>
2022-07-03 07:56:13 +0000

    symmetric-encrypt ADD

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 pub mod hash;
 pub mod salt;
 pub mod hmac;
-// #[path="../src/symmetric-encrypt.rs"]
-// pub mod symmetric_encrypt;
+#[path="../src/symmetric-encrypt.rs"]
+pub mod symmetric_encrypt;
 
 extern crate hex;
 
 
 fn main() {
-    let mut line = String::new();
+    // let mut line = String::new();
    
-    println!("Enter a string :");
+    // println!("Enter a string :");
    
-    let _b1 = std::io::stdin().read_line(&mut line).unwrap();
+    // let _b1 = std::io::stdin().read_line(&mut line).unwrap();
 
-    println!("{}", hash::hash(line.clone()));
+    // println!("{}", hash::hash(line.clone()));
 
-    println!("{}", salt::signup(format!("{}@email.com", line.trim_end()), line.clone()));
-    println!("{}", salt::login(format!("{}@email.com", line.trim_end()), line.clone()));
+    // println!("{}", salt::signup(format!("{}@email.com", line.trim_end()), line.clone()));
+    // println!("{}", salt::login(format!("{}@email.com", line.trim_end()), line.clone()));
 
-    hmac::hmac();
+    // hmac::hmac();
+
+    symmetric_encrypt::encrypt();
 }
